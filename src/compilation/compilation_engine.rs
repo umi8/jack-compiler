536d4e634be3c2b0dfccf3d9b3927c73fa66b56a
Hiromi Furukawa <umi8@users.noreply.github.com>
2023-01-20 08:58:18 +0900

    feat: copy from chapter 10 repository

---
+++ src/compilation/compilation_engine.rs
@@ -0,0 +1,31 @@
+use std::io::Write;
+
+use anyhow::Result;
+
+use crate::compilation::class_compiler::ClassCompiler;
+use crate::compilation::xml_writer::XmlWriter;
+use crate::tokenizer::jack_tokenizer::JackTokenizer;
+
+pub trait CompilationEngine {
+    fn new(tokenizer: JackTokenizer) -> Self;
+    fn compile(&mut self, writer: &mut impl Write) -> Result<()>;
+}
+
+pub struct XmlCompilationEngine {
+    tokenizer: JackTokenizer,
+    writer: XmlWriter,
+}
+
+impl CompilationEngine for XmlCompilationEngine {
+    fn new(tokenizer: JackTokenizer) -> Self {
+        XmlCompilationEngine {
+            tokenizer,
+            writer: XmlWriter::new(),
+        }
+    }
+
+    fn compile(&mut self, writer: &mut impl Write) -> Result<()> {
+        ClassCompiler::compile(&mut self.tokenizer, &mut self.writer, writer)?;
+        Ok(())
+    }
+}
